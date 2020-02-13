d565917ee2935ecc1afef8f1a3f1b1f20b1bc975
lionelschiepers <lionel_schiepers@hotmail.com>
2020-02-13 14:45:38 +0100

    first try
    firstry

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YahooFinance from "./Components/YahooFinance";
+import axios from 'axios';
+/*
+const yahooFinanceHttp = axios.create
+(
+  {
+    baseURL:'https://query1.finance.yahoo.com',
+    headers: {'X-Requested-With': 'XMLHttpRequest'},
+}
+);
+*/
+const yahooFinanceHttp = axios.create
+(
+  {
+    baseURL:'https://cors-anywhere.herokuapp.com'
+}
+);
+/*
+const yahooFinanceHttp = axios.create(
+  {
+    headers: {'X-Requested-With': 'XMLHttpRequest'},
+  }
+);
+*/
 
 function App() {
+
+    var cors_api_url = '/https://query1.finance.yahoo.com/v7/finance/quote?symbols=AAPL';
+ 
+  /*
+  let data2 = yahooFinanceHttp
+    .get('/stockApi')
+    .then(response => response.data);
+*/
+  let data = yahooFinanceHttp
+    //.get('/v7/finance/quote?symbols=AAPL')
+    .get(cors_api_url)
+    .then(response => response.data);
+  /*console.log(data);
+*/
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/Components/YahooFinance.js
@@ -0,0 +1,7 @@
+const yahooFinanceUrl = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=AAPL";
+
+function YahooFinance() 
+{
+    return "<div>mydiv</div>";
+}
+export default YahooFinance;
