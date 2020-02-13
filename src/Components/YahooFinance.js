1336f8729055be52aa8517803b3b0d2a5da973d3
lionelschiepers <lionel_schiepers@hotmail.com>
2020-02-13 17:11:25 +0100

    the stock price of apple is displayed!
    the stock price of apple is displayed!

--- src/Components/YahooFinance.js
+++ src/Components/YahooFinance.js
@@ -1,7 +1,48 @@
-const yahooFinanceUrl = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=AAPL";
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function YahooFinance() 
+const anyCorsHttp = axios.create(
+  { baseURL:'https://cors-anywhere.herokuapp.com' }
+);
+
+const yahooFinanceUrl = 'https://query1.finance.yahoo.com/v7/finance/quote';
+
+function getUrl(quote)
 {
-    return "<div>mydiv</div>";
+    return yahooFinanceUrl + '?symbols=' + quote;
 }
+
+class YahooFinance extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            yahooData : {}
+        }
+      }
+
+      componentDidMount() {
+        anyCorsHttp
+            .get('/' + getUrl('AAPL'))
+            .then(json => {
+                this.setState({
+                    yahooData : json.data
+                    })
+                });
+      }
+    
+    render() {
+        const state = this.state;
+        let json = '';
+        try {
+            json = JSON.stringify(state.yahooData.quoteResponse.result[0].regularMarketPrice);
+
+        } catch(error) {
+            json = 'error';
+        }
+        return (
+            <div>Apple data:{json}</div>
+        );
+    }
+}
+
 export default YahooFinance;
