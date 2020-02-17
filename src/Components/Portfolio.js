b03f86f80eb2beaed568b6b26c4df2bd61cfa52d
lionelschiepers <lionel_schiepers@hotmail.com>
2020-02-17 17:27:34 +0100

    the parsing of CSV is working

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { csvParse } from 'csv-parse/lib/sync';
+import parser  from 'csv-parse/lib/sync';
 import axios from 'axios';
 
 
@@ -8,27 +8,14 @@ export class Portfolio
     // Loads the specified transactions
     // sample file: https://raw.githubusercontent.com/lionelschiepers/MyStock/master/MyStockWeb/Data/1.csv
     Load = async(url) => {
-        const stream = csvParse.parse({});
-        let test =  csvParse.parse('TOTO', {});
+        
         const result = [];
-        /*
-        const parser = csvParser({separator: ','});
-
-          parser.on('readable', () => {
-            let record
-            while (record = parser.read()) 
-            {
-              output.push(record)
-            }
-          });
-*/
           axios
             .get(url)
-      .then(res => {
-          
-        const data = csvParse.parse(res.data, {});
-        result.push(data);
-      
+            .then(res => {
+                const data = parser(res.data, {columns:true});
+                result.push(...data);
+ 
       });
       
     }
