92f9719547d94667206962f1a62574cbc8991287
lionelschiepers <lionel_schiepers@hotmail.com>
2020-02-18 14:41:53 +0100

    Updates

---
+++ precache-manifest.3b2175fdee3a63d56d99ec65d2755184.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a27cba4045e0faec8ea361ca136258",
+    "url": "/ReactStockQuote/index.html"
+  },
+  {
+    "revision": "4e0c164e67159624bc88",
+    "url": "/ReactStockQuote/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "e45b7758d295c6d00616",
+    "url": "/ReactStockQuote/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "4e0c164e67159624bc88",
+    "url": "/ReactStockQuote/static/js/2.6b3df577.chunk.js"
+  },
+  {
+    "revision": "c47762924c431f97af69157911a22d21",
+    "url": "/ReactStockQuote/static/js/2.6b3df577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45b7758d295c6d00616",
+    "url": "/ReactStockQuote/static/js/main.d49b0c3f.chunk.js"
+  },
+  {
+    "revision": "d0be4c3177df17c3becf",
+    "url": "/ReactStockQuote/static/js/runtime-main.c4d13ba9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactStockQuote/static/media/logo.ee7cd8ed.svg"
+  }
+]);
