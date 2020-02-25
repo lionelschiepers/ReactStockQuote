64b965e1a462ec7beb206cfcc3f41f98c8dd435e
lionelschiepers <lionel_schiepers@hotmail.com>
2020-02-25 12:10:51 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactStockQuote/precache-manifest.1c2ea04e59cb71f56a357e1cf88c43e0.js"
+  "/ReactStockQuote/precache-manifest.56a24eeebf421cff614e9a51e62f409d.js"
 );
 
 self.addEventListener('message', (event) => {
