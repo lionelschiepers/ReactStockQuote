afec8f62be635813a95a00ddca4832c603d53e4e
Lionel Schiepers <lionel_schiepers@hotmail.com>
2022-03-03 18:33:30 +0100

    code cleanup

--- api/exchange-rate-ecb/index.ts
+++ api/exchange-rate-ecb/index.ts
@@ -1,38 +1,50 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import axios from "axios";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> 
-{
-    context.log('HTTP trigger GetExchangeRates launched');
-    try
-    {
-        const response = await axios.get<string>("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-        const responseMessage = response.data;
-        const responseContentType = response.headers["content-type"];
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.log("HTTP trigger GetExchangeRates launched");
+  try {
+    const response = await axios.get<string>(
+      "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+    );
+    const responseMessage = response.data;
+    const responseContentType = response.headers["content-type"];
 
-        context.res = 
-        {
-            status: response.status,
-            body: responseMessage,
-            headers: 
-            {
-                'Content-Type': responseContentType,
-                'Cache-Control': 'max-age=86400',
-                'Access-Control-Allow-Origin': '*',
-                "Access-Control-Allow-Methods": "GET"
-            }
-        };
-    }
-    catch(e)
-    {
-        context.res = 
-        {
-            status: e.response.status,
-            body: e.response.statusText
-        };
+    context.res = {
+      status: response.status,
+      body: responseMessage,
+      headers: {
+        "Content-Type": responseContentType,
+        "Cache-Control": "max-age=86400",
+        //                'Access-Control-Allow-Origin': '*',
+        //                "Access-Control-Allow-Methods": "GET"
+      },
+    };
 
-        context.log.error(e);
+        // code to refactor for production and use application settings
+
+    let origin = req.headers["origin"] || req.headers["Origin"];
+    const cors_domains = [
+      "https://zealous-pebble-0e123ed03.1.azurestaticapps.net/",
+      "http://localhost:3000",
+      "http://localhost:7071"
+    ];
+
+    if (origin != null && cors_domains.indexOf(origin) >= 0) {
+      context.res.headers["Access-Control-Allow-Origin"] = origin;
+      context.res.headers["Access-Control-Allow-Methods"] = "GET";
     }
+  } catch (e) {
+    context.res = {
+      status: e.response.status,
+      body: e.response.statusText,
+    };
+
+    context.log.error(e);
+  }
 };
 
 export default httpTrigger;
--- api/yahoo-finance/index.ts
+++ api/yahoo-finance/index.ts
@@ -1,44 +1,60 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import axios from "axios";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> 
-{
-    context.log('HTTP trigger YahooFinance launched');
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.log("HTTP trigger YahooFinance launched");
 
-    try
-    {
-        // sample url https://query1.finance.yahoo.com/v7/finance/quote?symbols=UNA.AS&fields=regularMarketPrice,regularMarketPreviousClose,trailingAnnualDividendRate
+  try {
+    // sample url https://query1.finance.yahoo.com/v7/finance/quote?symbols=UNA.AS&fields=regularMarketPrice,regularMarketPreviousClose,trailingAnnualDividendRate
 
-        const queryString = Object.keys(req.query).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(req.query[key])).join('&');
-        const baseUrl = 'https://query1.finance.yahoo.com/v7/finance/quote';
-        const requestUrl = baseUrl + '?' + queryString;
+    const queryString = Object.keys(req.query)
+      .map(
+        (key) =>
+          encodeURIComponent(key) + "=" + encodeURIComponent(req.query[key])
+      )
+      .join("&");
+    const baseUrl = "https://query1.finance.yahoo.com/v7/finance/quote";
+    const requestUrl = baseUrl + "?" + queryString;
 
-        // https://blog.logrocket.com/async-await-in-typescript/
-        const response = await axios.get<string>(requestUrl);
+    // https://blog.logrocket.com/async-await-in-typescript/
+    const response = await axios.get<string>(requestUrl);
 
-        const responseMessage = response.data;
-        const responseContentType = response.headers["content-type"];
-    
-        context.res = {
-            status: response.status,
-            body: responseMessage,
-            headers: {
-                'Content-Type': responseContentType,
-                'Access-Control-Allow-Origin': '*',
-                "Access-Control-Allow-Methods": "GET"
-            }
-        };
+    const responseMessage = response.data;
+    const responseContentType = response.headers["content-type"];
+
+    context.res = {
+      status: response.status,
+      body: responseMessage,
+      headers: {
+        "Content-Type": responseContentType,
+//        "Access-Control-Allow-Origin": "*",
+//        "Access-Control-Allow-Methods": "GET",
+      },
+    };
+
+    // code to refactor for production and use application settings
+    let origin = req.headers["origin"] || req.headers["Origin"];
+    const cors_domains = [
+        "https://zealous-pebble-0e123ed03.1.azurestaticapps.net/",
+        "http://localhost:3000",
+        "http://localhost:7071"
+      ];
+
+    if (origin != null && cors_domains.indexOf(origin) >= 0) {
+      context.res.headers["Access-Control-Allow-Origin"] = origin;
+      context.res.headers["Access-Control-Allow-Methods"] = "GET";
     }
-    catch(e)
-    {
-        context.res = 
-        {
-            status: e.response.status,
-            body: e.response.statusText
-        };
+  } catch (e) {
+    context.res = {
+      status: e.response.status,
+      body: e.response.statusText,
+    };
 
-        context.log.error(e);
-    }
+    context.log.error(e);
+  }
 };
 
 export default httpTrigger;
