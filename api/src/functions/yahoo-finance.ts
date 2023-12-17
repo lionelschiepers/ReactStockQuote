77b561d6ea0d3730570947eeb5335dccf45c4b9a
Lionel Schiepers <lionel_schiepers@hotmail.com>
2023-12-17 22:07:05 +0100

    use new yahoo finance package

--- api/src/functions/yahoo-finance.ts
+++ api/src/functions/yahoo-finance.ts
@@ -2,6 +2,7 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 import axios from "axios";
 import yahooFinance2 from 'yahoo-finance2';
 
+// sample call: http://localhost:7071/api/yahoo-finance?symbols=MSFT&fields=regularMarketPrice
 export async function yahooFinance(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log("HTTP trigger YahooFinance launched");
 
@@ -10,29 +11,34 @@ export async function yahooFinance(request: HttpRequest, context: InvocationCont
 
     const symbols = request.query.get("symbols").split(",");
     const fields = request.query.get("fields").split(",");
-    const results = await yahooFinance2.quote(symbols, {fields:fields});
-
-    const queryString = Object.keys(request.query)
-      .map(
-        (key) =>
-          encodeURIComponent(key) + "=" + encodeURIComponent(request.query[key])
-      )
-      .join("&");
-    const baseUrl = "https://query1.finance.yahoo.com/v6/finance/quote";
-    const requestUrl = baseUrl + "?" + queryString;
-
-    // https://blog.logrocket.com/async-await-in-typescript/
-    const response = await axios.get<string>(requestUrl);
-
-    const responseMessage = response.data;
+    const results = await yahooFinance2.quote(symbols, { fields: fields });
+    /*
+        const queryString = Object.keys(request.query)
+          .map(
+            (key) =>
+              encodeURIComponent(key) + "=" + encodeURIComponent(request.query[key])
+          )
+          .join("&");
+        const baseUrl = "https://query1.finance.yahoo.com/v6/finance/quote";
+        const requestUrl = baseUrl + "?" + queryString;
+    
+        // https://blog.logrocket.com/async-await-in-typescript/
+        const response = await axios.get<string>(requestUrl);
+    
+        const responseMessage = response.data;
     const responseContentType = response.headers["content-type"];
+      */
+    const responseMessage = results;
+    const responseContentType = "application/json";
 
     return {
-      status: response.status,
-      body: responseMessage,
+//      status: response.status,
+      status: 200,
+      jsonBody: responseMessage
+      /*,
       headers: {
         "Content-Type": responseContentType
-      },
+      },*/
     };
   } catch (e) {
     return {
