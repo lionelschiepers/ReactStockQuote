cb9fe5c9c1dc9992bf9bde9ca7946609e0419e16
Lionel Schiepers <lionel_schiepers@hotmail.com>
2023-12-19 19:39:55 +0100

    removed deprecated reactdom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -39,14 +38,6 @@ root.render(
 </Auth0Provider>,
 );
 
-/*
-ReactDOM.render(
-    <Auth0Provider {...providerConfig}>
-      <App />
-    </Auth0Provider>,
-    document.getElementById("root")
-  );
-*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
