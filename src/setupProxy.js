57448b1e197d4dd207d08522847f25bedb1b0e5f
Lionel Schiepers <lionel_schiepers@hotmail.com>
2024-05-04 15:40:57 +0200

    use proxy to call function locally

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,13 @@
 const helmet = require('helmet')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(helmet());
+    app.use(
+        '/api',
+        createProxyMiddleware({
+          target: 'http://localhost:7071/api',
+          changeOrigin: true,
+        })
+      );
 };
